eb5190ac8b0717ae93ade004d5ba7bef3522c19a
Daouda DIOUF <daoudaviddiouf@gmail.com>
2022-03-23 14:28:13 +0000

    test git issues

--- frontend/TenetNgMaterial/src/app/app.component.ts
+++ frontend/TenetNgMaterial/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isGranted?: boolean;
   title = 'TenetNgMaterial';
+
+  constructor(private ){
+
+  }
 }
